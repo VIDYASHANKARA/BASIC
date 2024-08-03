ba7e25df4d1578634e225d519ab38558d42ba4d6
VIDYASHANKARA C K <vidyashankara2005@gmail.com>
2024-08-03 17:43:04 +0530

    Update Sum.c

--- Sum.c
+++ Sum.c
@@ -17,3 +17,28 @@ int main()
     return 0;
 }
 
+/*****
+
+Write a program to calculate the sum of the numbers occurring in the
+multiplication table of 8.
+
+#include <stdio.h>
+
+int main()
+{
+    int n,sum=0,i=0;
+    printf("Enter the number n: ");
+    scanf("%d",&n);
+    while(i<n+1)
+    {
+        i*=8;
+        sum+=i;
+        i/=8;
+        i++;
+    }
+    printf("%d",sum);
+    return 0;
+}
+
+
+*****/
