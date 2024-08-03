676845f29b0c57831306d9b8721d4c27ab312646
VIDYASHANKARA C K <vidyashankara2005@gmail.com>
2024-08-03 14:22:46 +0530

    Create Lowercase?.c

---
+++ Lowercase?.c
@@ -0,0 +1,99 @@
+/******************************************************************************
+Write a program to determine whether a character entered by the user is
+lowercase or not.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    printf("Enter the character: ");
+    scanf("%c",&ch);
+    switch(ch)
+    {
+        case 'a':
+            printf("It is lowercase!");
+            break;
+        case 'b':
+            printf("It is lowercase!");
+            break;
+        case 'c':
+            printf("It is lowercase!");
+            break;
+        case 'd':
+            printf("It is lowercase!");
+            break;
+        case 'e':
+            printf("It is lowercase!");
+            break;
+        case 'f':
+            printf("It is lowercase!");
+            break;
+        case 'g':
+            printf("It is lowercase!");
+            break;
+        case 'h':
+            printf("It is lowercase!");
+            break;
+        case 'i':
+            printf("It is lowercase!");
+            break;
+        case 'j':
+            printf("It is lowercase!");
+            break;
+        case 'k':
+            printf("It is lowercase!");
+            break;
+        case 'l':
+            printf("It is lowercase!");
+            break;
+        case 'm':
+            printf("It is lowercase!");
+            break;
+        case 'n':
+            printf("It is lowercase!");
+            break;
+        case 'o':
+            printf("It is lowercase!");
+            break;
+        case 'p':
+            printf("It is lowercase!");
+            break;
+        case 'q':
+            printf("It is lowercase!");
+            break;
+        case 'r':
+            printf("It is lowercase!");
+            break;
+        case 's':
+            printf("It is lowercase!");
+            break;
+        case 't':
+            printf("It is lowercase!");
+            break;
+        case 'u':
+            printf("It is lowercase!");
+            break;
+        case 'v':
+            printf("It is lowercase!");
+            break;
+        case 'w':
+            printf("It is lowercase!");
+            break;
+        case 'x':
+            printf("It is lowercase!");
+            break;
+        case 'y':
+            printf("It is lowercase!");
+            break;
+        case 'z':
+            printf("It is lowercase!");
+            break;
+        default:
+            printf("It is not lowercase!");
+    }
+    
+    return 0;
+}
+
